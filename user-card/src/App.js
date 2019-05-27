938586d9c6de933b88fef1549f3e0a6a71c53b16
Safdar Jamal <safdar.dev@gmail.com>
2019-05-28 01:40:59 +0500

    Added user-card

---
+++ user-card/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import user from './images/user.png';
+import './App.css';
+import { createStore } from 'redux';
+import reducer from './reducers/index';
+
+const initialState = {
+  name: 'Batman',
+  description: 'Software Engineer, Speaker, and Occasional Model',
+  likes: 'Computers and Black Coffee',
+  location: 'localhost'
+};
+const store = createStore(reducer, initialState);
+
+function App() {
+  const { name, description, likes, location } = store.getState();
+
+  return (
+    <div className="App">
+      <section className="User__img">
+        <img src={user} alt="user" />
+      </section>
+
+      <section className="User__info">
+        <p>
+          {' '}
+          <span className="faint">I am</span> a {description}
+        </p>
+        <p>
+          {' '}
+          <span className="faint">I like</span> {likes}
+        </p>
+
+        <p className="User__info__details User__info__divider faint">
+          <span>Name: </span>
+          <span>{name}</span>
+        </p>
+        <p className="User__info__details faint">
+          <span>Location: </span>
+          <span>{location}</span>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
